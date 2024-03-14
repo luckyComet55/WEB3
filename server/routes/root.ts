c086b0b1b1c466fa49f315feafc560bd5bf7d551
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2024-03-15 02:13:51 +0300

    Remove server (too late)

--- server/routes/root.ts
+++
@@ -1,7 +0,0 @@
-import express, { Express, Router, Request, Response } from "express";
-
-const router: Router = Router().get('/', (req: Request, res: Response) => {
-  res.send('Hello!!!')
-})
-
-export default router
